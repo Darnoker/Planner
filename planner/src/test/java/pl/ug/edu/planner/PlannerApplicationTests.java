8de849b356f6461856dddbb5a7629df2389d51de
Konrad Głąb <73139334+Darnoker@users.noreply.github.com>
2023-11-10 12:02:15 +0100

    add initial project (#1)

---
+++ planner/src/test/java/pl/ug/edu/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ug.edu.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
