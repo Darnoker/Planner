30ab2030426156897fc7328b66d5d52063372547
Konrad Głąb <73139334+Darnoker@users.noreply.github.com>
2024-01-03 20:22:55 +0100

    angular time table (#12)
    * angular
    
    * Dobra jest home pog
    
    * timetable/login/register
    
    ---------
    
    Co-authored-by: AleksandraJ <jankowskaaaleksandra01@gmail.com>
    Co-authored-by: AleksandraJ01 <108745697+AleksandraJ01@users.noreply.github.com>

--- angular/Planner/src/app/app-routing.module.ts
+++ angular/Planner/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TimetableComponent } from './timetable/timetable.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  { path: 'timetable', component: TimetableComponent },
+  { path: '', redirectTo: '/timetable', pathMatch: 'full' },
 ];
 
 @NgModule({
--- angular/Planner/src/app/app.module.ts
+++ angular/Planner/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainContainerComponent } from './main-container/main-container.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TimetableComponent } from './timetable/timetable.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,11 +19,16 @@ import { HomeComponent } from './home/home.component';
     HeaderComponent,
     FooterComponent,
     MainContainerComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    TimetableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
