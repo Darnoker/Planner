23b21a042a3e3ae8986909a6830ed1b06f69cc0c
Tenan <Mateusz.Gietek@gmail.com>
2024-01-21 06:01:23 +0100

    Login, trochę authentykacja, Tokeny

--- angular/Planner/src/app/defs/authentication-defs.ts
+++ angular/Planner/src/app/defs/authentication-defs.ts
@@ -2,3 +2,8 @@ export interface RegisterRequest {
   email: string;
   password: string;
 }
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
---
+++ planner/src/main/java/pl/ug/edu/planner/authentication/request/TokenRequest.java
@@ -0,0 +1,14 @@
+package pl.ug.edu.planner.authentication.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenRequest {
+    private String token;
+}
